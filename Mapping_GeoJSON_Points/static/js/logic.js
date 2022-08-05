7e8834169a985aebc9102745f9ab9981d0469491
Maria Sevillano <mariasevillano@Marias-MBP.attlocal.net>
2022-08-05 11:07:35 -0500

    Adding other cases

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -57,3 +57,13 @@ let sanFranAirport =
 // }).addTo(map);
 
 // MAPPING MULTIPLE JSON POINTS
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
